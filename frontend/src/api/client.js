7e1740aeb17dad740c3ee271d21241b444779d56
Yash9385 <yashsingh0120@gmail.com>
2026-05-07 14:30:00 +0530

    fix production api url

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'https://task-manager-0k1j.onrender.com/api',
 });
 
 client.interceptors.request.use((config) => {
